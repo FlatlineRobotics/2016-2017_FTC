8d94569b75f681babb15ecf1e08babceecc3a13d
Elias-Sheumaker <eliass@rvmt.org>
2016-10-01 20:40:29 -0500

    First steps towards a unified OpMode.

---
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/BellsAndWhistles.java
@@ -0,0 +1,28 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.ftcrobotcontroller.opmodes.ArcadeDrive;
+import com.qualcomm.ftcrobotcontroller.opmodes.TankDrive;
+
+/**
+ * Created on 10/1/2016.
+ */
+
+public class BellsAndWhistles extends OpMode {
+
+    @Override
+    public void init(){
+
+
+        }
+
+    @Override
+    public void loop(){
+
+        boolean rBump = gamepad1.left_bumper;
+
+        if (rBump == true) ;
+
+    }
+
+}
